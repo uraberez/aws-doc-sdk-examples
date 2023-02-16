399123c64ca442cf890de63c7116eaa23bb0fd30
Yuriy Berezin <yuriy.berezin@innovecs.com>
2023-02-16 16:54:53 +0200

    CreateBucket modified

--- javav2/example_code/s3/src/main/java/com/example/s3/CreateBucket.java
+++ javav2/example_code/s3/src/main/java/com/example/s3/CreateBucket.java
@@ -10,71 +10,67 @@
 package com.example.s3;
 
 // snippet-start:[s3.java2.create_bucket_waiters.import]
+
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.waiters.WaiterResponse;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
-import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
-import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
-import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.services.s3.waiters.S3Waiter;
+
 import java.net.URISyntaxException;
 // snippet-end:[s3.java2.create_bucket_waiters.import]
-
-/**
- * Before running this Java V2 code example, set up your development environment, including your credentials.
- *
- * For more information, see the following documentation topic:
- *
- * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
- */
 
 public class CreateBucket {
 
     public static void main(String[] args) throws URISyntaxException {
 
         final String usage = "\n" +
-            "Usage:\n" +
-            "    <bucketName> \n\n" +
-            "Where:\n" +
-            "    bucketName - The name of the bucket to create. The bucket name must be unique, or an error occurs.\n\n" ;
+                "Usage:\n" +
+                "    <bucketName> \n\n" +
+                "Where:\n" +
+                "    bucketName - The name of the bucket to create. The bucket name must be unique, or an error occurs.\n\n";
 
-       if (args.length != 1) {
-           System.out.println(usage);
-           System.exit(1);
-       }
+        if (args.length != 1) {
+            System.out.println(usage);
+            System.exit(1);
+        }
 
         String bucketName = args[0];
         System.out.format("Creating a bucket named %s\n", bucketName);
         ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create("dev");
         Region region = Region.EU_CENTRAL_1;
         S3Client s3 = S3Client.builder()
-            .region(region)
-            .credentialsProvider(credentialsProvider)
-            .build();
+                .region(region)
+                .credentialsProvider(credentialsProvider)
+                .build();
 
-        createBucket (s3, bucketName);
+        // List buckets
+        ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
+        ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
+        listBucketsResponse.buckets().forEach(x -> System.out.println(x.name()));
+
+        createBucket(s3, bucketName);
         s3.close();
     }
 
     // snippet-start:[s3.java2.create_bucket_waiters.main]
-    public static void createBucket( S3Client s3Client, String bucketName) {
+    public static void createBucket(S3Client s3Client, String bucketName) {
         try {
             S3Waiter s3Waiter = s3Client.waiter();
             CreateBucketRequest bucketRequest = CreateBucketRequest.builder()
-                .bucket(bucketName)
-                .build();
+                    .bucket(bucketName)
+                    .build();
 
             s3Client.createBucket(bucketRequest);
             HeadBucketRequest bucketRequestWait = HeadBucketRequest.builder()
-                .bucket(bucketName)
-                .build();
+                    .bucket(bucketName)
+                    .build();
 
             // Wait until the bucket is created and print out the response.
             WaiterResponse<HeadBucketResponse> waiterResponse = s3Waiter.waitUntilBucketExists(bucketRequestWait);
             waiterResponse.matched().response().ifPresent(System.out::println);
-            System.out.println(bucketName +" is ready");
+            System.out.println(bucketName + " is ready");
 
         } catch (S3Exception e) {
             System.err.println(e.awsErrorDetails().errorMessage());
